9e41cfa45469cab78c1f490d18733889e2d1c168
Zeesat <alauddinadhaps@gmail.com>
2026-05-19 06:18:25 +0700

    feat(start): add starting screen flow before entering world

---
+++ src/fantasyrpg/ui/world/WorldFrame.java
@@ -0,0 +1,28 @@
+package fantasyrpg.ui.world;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+public class WorldFrame extends JFrame {
+    public WorldFrame() {
+        super("Fantasy RPG");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(true);
+
+        GamePanel gamePanel = new GamePanel();
+
+        setContentPane(gamePanel);
+        pack();
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        gamePanel.requestFocusInWindow();
+        gamePanel.startGameThread();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(WorldFrame::new);
+    }
+}
